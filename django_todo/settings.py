8ef0edb85e6e954ec91323e49bbbc5e34f6bcc51
Ankita Ohwal <ohalankita29@gmail.com>
2023-12-24 12:50:28 +0530

    Preparing for Heroku deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,6 +86,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+DATABASES['default'] = dj_database_url.config(default="sqlite:///todo.db")
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Password validation
